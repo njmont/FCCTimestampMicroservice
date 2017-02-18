70a1f8c474c1533d04cbc1b5abea4d7c0d00722c
Nathan Monte <nathan.monte.kh@gmail.com>
2017-02-18 10:17:44 +0000

    Initialcommit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.param('date', function(req, res, next, value) {
+  "use strict";
+
+  let return_object = {
+    unix: null,
+    natural: null
+  }
+  if (Number(value)) {
+    value = Number(value) * 1000;
+  }
+
+  let date = new Date(value);
+  if (date != "Invalid Date") {
+    return_object.unix = Math.round(date.getTime() / 1000);
+    return_object.natural = date.toDateString();
+  } else {
+    console.log("Invalid date");
+  }
+  
+  res.json(return_object);
+});
+
+router.get("/:date", function(req, res, next){
+})
+
+module.exports = router;
